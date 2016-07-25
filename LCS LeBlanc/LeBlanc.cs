3b4baaec9c3d3af462a6a328e81cc3f4a0cdb00a
Hikigaya <otakusekainet@gmail.com>
2016-07-25 03:19:23 +0300

    LCS LEBLANC

---
+++ LCS LeBlanc/LeBlanc.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LCS_LeBlanc.Extensions;
+using LCS_LeBlanc.Modes;
+using LCS_LeBlanc.Modes.Combo;
+using LeagueSharp.Common;
+using LeagueSharp;
+
+namespace LCS_LeBlanc
+{
+    public class LeBlanc
+    {
+        public LeBlanc()
+        {
+            OnLoad();
+        }
+
+        private static void OnLoad()
+        {
+            Spells.Initialize();
+            Menus.Initialize();
+
+            Game.OnUpdate += OnUpdate;
+            AntiGapcloser.OnEnemyGapcloser += OnGapcloser;
+        }
+        private static void OnGapcloser(ActiveGapcloser gapcloser)
+        {
+            if (gapcloser.Sender.IsEnemy && gapcloser.Sender.IsValidTarget(Spells.E.Range) &&
+                (gapcloser.Sender.Target.IsMe || ObjectManager.Player.Distance(gapcloser.End) < 100) && Spells.E.IsReady()
+                && Utilities.Enabled("anti-gapcloser.e"))
+            {
+                Spells.E.Cast(gapcloser.Sender.Position);
+            }
+        }
+
+        private static void ComboSelector()
+        {
+            switch (Menus.Config.Item("combo.mode").GetValue<StringList>().SelectedIndex)
+            {
+                case 0:
+                    QRWE.Init();
+                    break;
+                case 1:
+                    WRQE.WRQECombo();
+                    break;
+            }
+        }
+
+        private static void OnUpdate(EventArgs args)
+        {
+           
+            Utilities.UpdateUltimateVariable();
+
+            switch (Menus.Orbwalker.ActiveMode)
+            {
+                case Orbwalking.OrbwalkingMode.Combo:
+                    ComboSelector();
+                    break;
+                case Orbwalking.OrbwalkingMode.Mixed:
+                    Mixed.Init();
+                    break;
+                case Orbwalking.OrbwalkingMode.LaneClear:
+                    Clear.WaveInit();
+                    Clear.JungleInit();
+                    break;
+            }
+        }
+
+    }
+}
---
+++ LCS LeBlanc/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace LCS_LeBlanc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += OnLoad;
+        }
+
+        private static void OnLoad(EventArgs args)
+        {
+            if (ObjectManager.Player.ChampionName != "Leblanc")
+            {
+                return;
+            }
+            else
+            {
+                // ReSharper disable once ObjectCreationAsStatement
+                new LeBlanc();
+            }
+        }
+    }
+}
