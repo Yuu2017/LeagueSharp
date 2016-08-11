1312bbb6c249c7fe0fd3c7684ebc84a4d7265b59
Hikigaya <otakusekainet@gmail.com>
2016-08-11 06:22:12 +0300

    Hiki Kled Release

---
+++ Hiki.Kled/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using Hiki.Kled.Champions;
+
+namespace Hiki.Kled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += OnLoad;
+        }
+
+        private static void OnLoad(EventArgs args)
+        {
+            switch (ObjectManager.Player.ChampionName)
+            {
+                case "Kled":
+                    new Champions.Kled();
+                    break;
+            }
+        }
+    }
+}
