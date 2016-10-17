eb3d1da86dd18c426579ad2d736428bbf392872f
Hikigaya <otakusekainet@gmail.com>
2016-10-18 00:52:13 +0300

    OKTW PREDICTIONER

---
+++ OKTWPredictioner/EventHandlers.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SebbyLib;
+
+namespace OKTWPredictioner
+{
+    public class EventHandlers
+    {
+        private static bool[] handleEvent = { true, true, true, true };
+
+        public static void Game_OnGameLoad(EventArgs args)
+        {
+            OKTWPredictioner.Initialize();
+        }
+
+        public static void Obj_AI_Hero_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        {
+            if (sender.IsMe)
+            {
+                SpellSlot slot = ObjectManager.Player.GetSpellSlot(args.SData.Name);
+                if (!Utility.IsValidSlot(slot))
+                    return;
+
+                if (!handleEvent[(int)slot])
+                {
+                    if (OKTWPredictioner.Spells[(int)slot] != null)
+                        handleEvent[(int)slot] = true;
+                }
+            }
+        }
+
+        public static void Spellbook_OnCastSpell(Spellbook sender, SpellbookCastSpellEventArgs args)
+        {
+            if (sender.Owner.IsMe)
+            {
+                if (OKTWPredictioner.Config.Item("ENABLED").GetValue<bool>() && (OKTWPredictioner.Config.Item("COMBOKEY").GetValue<KeyBind>().Active || OKTWPredictioner.Config.Item("HARASSKEY").GetValue<KeyBind>().Active))
+                {
+                    if (!Utility.IsValidSlot(args.Slot))
+                        return;
+
+                    if (OKTWPredictioner.Spells[(int)args.Slot] == null)
+                        return;
+
+                    if (!OKTWPredictioner.Config.Item(String.Format("{0}{1}", ObjectManager.Player.ChampionName, args.Slot)).GetValue<bool>())
+                        return;
+
+                    if (handleEvent[(int)args.Slot])
+                    {
+                        args.Process = false;
+                        handleEvent[(int)args.Slot] = false;
+                        var enemy = args.EndPosition.GetEnemiesInRange(200f).OrderByDescending(p => Utility.GetPriority(p.ChampionName)).FirstOrDefault();
+                        if (enemy == null)
+                            enemy = TargetSelector.GetTarget(OKTWPredictioner.Spells[(int)args.Slot].Range, TargetSelector.DamageType.Physical);
+
+
+
+                        if (enemy != null)
+                        {
+                            OKTWPredictioner.Spells[(int)args.Slot].CastSebby(enemy, Utility.HitchanceArray[OKTWPredictioner.Config.Item("HITCHANCE").GetValue<StringList>().SelectedIndex]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
